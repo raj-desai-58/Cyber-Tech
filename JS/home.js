275307ac5b2f5c3992b22417d73218f3902e04cd
Raj Desai <140167428+raj-desai-58@users.noreply.github.com>
2023-08-10 16:21:50 +0530

    added 2 javascript files

---
+++ JS/home.js
@@ -0,0 +1,18 @@
+console.log("Welcome to Cyber Teche");
+console.log("Develop by Raj Desai");
+
+var typed = new Typed('#element', {
+    strings: ["Working Skills","Networking","Digital Marketing","Soft Skills","Lowgical Thinking"],
+    typeSpeed: 80,
+    loop: true,
+    onComplete: function(self) { self.cursor.remove() },
+});
+
+    var typed = new Typed('#element2', {
+    strings: ["Working Skills","Networking","Digital Marketing","Soft Skills","Lowgical Thinking"],
+    typeSpeed: 80,
+    loop: true,
+    onComplete: function(self) { self.cursor.remove() },
+});
+
+var links = document.getElementById("links");
